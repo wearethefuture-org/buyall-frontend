c3ef5ff94acf30d261d7072f06dedf073e92773d
Pavel <pavel.makk.old@gmail.com>
2019-11-07 18:12:58 +0200

    Delete password from user interface

--- src/app/core/interfaces/user.d.ts
+++ src/app/core/interfaces/user.d.ts
@@ -3,7 +3,6 @@ export interface IUser {
     firstName: string,
     lastName: string,
     email: string,
-    password: string,
     status: string,
     disabled: boolean,
     dateBirthday: string
